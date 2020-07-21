528cca07a169ec5bda3360247ca74bb9760b5d77
Tim Newbold <timnewbold.research@gmail.com>
2020-07-21 07:04:05 +0100

    Get rid of study check for public PREDICTS database

--- 1_PrepareDiversityData.R
+++ 1_PrepareDiversityData.R
@@ -16,14 +16,8 @@ cat('Loading database extracts\n')
 diversity<-readRDS(paste(dataDir,"database.rds",sep=""))
 
 cat('Selecting appropriate data\n')
-diversity <- diversity[apply(diversity[,c('Longitude','Latitude')],1,function(r) all(!is.na(r))),]
-
-# Drop some studies where the land-use classification is problematic
-problem.studies<-c("AD1_2011__Hanley","AD1_2011__Yoon","VK1_2007__StLaurent",
-                   "VK1_2013__ABMIboreal","SE1_2012__Rosselli","HW1_2008__Pillsbury",
-                   "MG1_2008__Boutin","MG1_2011__Schon","MG1_2012__Schmidt",
-                   "DI1_2013__Munyuli")
-diversity<-diversity[!(diversity$Source_ID %in% problem.studies),]
+diversity <- diversity[apply(diversity[,c('Longitude','Latitude')],1,
+                             function(r) all(!is.na(r))),]
 
 # Drop studies that focused on a single species
 diversity <- diversity[!diversity$Rank_of_study_common_taxon %in% 
